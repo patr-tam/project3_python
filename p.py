a0d875c9d08b4dd25ace558065c3dbe2d4ed6fb5
Patrick Tam <patr.tam@gmail.com>
2018-02-27 15:07:56 -0600

    Added ability to see whether file is in directory

--- p.py
+++ p.py
@@ -1,2 +1,10 @@
-##import urllib.request
-##urllib.request.urlretrieve ("https://s3.amazonaws.com/tcmg476/http_access_log", "log.txt")
+import urllib.request
+import os.path
+test=(os.path.isfile("log.txt"))
+if test is False:
+    urllib.request.urlretrieve ("https://s3.amazonaws.com/tcmg476/http_access_log", "log.txt")
+    if os.path.isfile("log.txt") is True:
+        print("Finished downloading")
+    
+else:
+    print (os.path.isfile("log.txt"))
